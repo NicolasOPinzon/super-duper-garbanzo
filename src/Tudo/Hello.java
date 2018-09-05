9607e4baf0c0ba05464652373caaf24325ac8e6c
Fabiano M. Dutra <fmbdutra@gmail.com>
2018-09-05 15:16:47 -0300

    #1

---
+++ src/Tudo/Hello.java
@@ -0,0 +1,15 @@
+package Tudo;
+
+/**
+ * 
+ * @author Nicolas Pinzon
+ * @version 1.0
+ */
+public class Hello {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hello world!");
+	}
+
+}
